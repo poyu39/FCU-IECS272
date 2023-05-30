7fd1692c195809c8d2f87deec4dc7032b493a1ef
Poyu <42506064+poyu39@users.noreply.github.com>
2023-05-30 14:34:38 +0800

    新增 Pillow

--- exam/djbmi/bmi/models.py
+++ exam/djbmi/bmi/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class People(models.Model):
+    photo = models.ImageField(upload_to='img/', blank=True, null=True)
     ssn = models.CharField(max_length=10)
     pname = models.CharField(max_length=30)
     tall = models.DecimalField(max_digits=4, decimal_places=1)
--- exam/djbmi/bmi/urls.py
+++ exam/djbmi/bmi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('people/', views.allpeople, name='index'),
     path('people/details/<slug:ssn>', views.details, name='details'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
